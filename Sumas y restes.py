118cc77f3dbadece16f4bf2e1857554e9abc5901
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-03-05 18:28:19 -0600

    Añadir variable Hola y función sumar en Sumas y restes.py

--- Sumas y restes.py
+++ Sumas y restes.py
@@ -1,16 +1,19 @@
+#Variables
 a = 0
 
 b = 10
 
-c=19
+c = 19
 
 d = 20
 
+Hola = "hola mundo"
 
+#Funciones
 def sumar(a, d, c, b):
     return a + d + c + b
 
-
+#Programa principal
 print("La suma de los valores es: ", sumar(a, d, c, b))
 
 
